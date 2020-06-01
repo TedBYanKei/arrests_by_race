2139565c70552fddfb88ce9ebe7fe0e2d5e1fef2
TedBYanKei <46034740+TedBYanKei@users.noreply.github.com>
2020-06-01 15:53:41 -0700

    Update arrests_by_race.r
    Updates to graph

--- arrests_by_race.r
+++ arrests_by_race.r
@@ -18,7 +18,7 @@ arrests_black_fmt <- arrests_black %>%
 arrests_all_fmt <- arrests_all %>%
   janitor::clean_names()%>% 
   mutate(arrests_pct = round((total_arrests / total_population), digits = 3)) %>% 
-  mutate(which = "All")  %>% 
+  mutate(which = "All Americans")  %>% 
   select(year, which, arrests_pct)
 
 arrests_fmt <- rbind(arrests_black_fmt, arrests_all_fmt)
@@ -30,9 +30,10 @@ arrests_fmt %>%
   geom_line(aes(col = which), size=1.2) + 
   scale_color_manual(values = brewer.pal(8,"Set1")) + 
   scale_y_continuous(labels = function(x) paste0(sprintf("%.0f", x*100),"%"), limits = c(0, 0.15), breaks = seq(0, 0.14, by = 0.02)) + 
-  labs(y="Percentage", x = "Year", caption = "source: https://www.bjs.gov/") + 
+  labs(y="Percentage", x = "Year", caption = "Source: US Dept of Justice - https://www.bjs.gov/") + 
   theme(axis.text.x = element_text(face="bold", angle = 50, hjust = 1), 
-        axis.text.y = element_text(face="bold")) +  
-  ggtitle("Arrest Percentage - # of Arrests Per Year Divided by Total Population")
+        axis.text.y = element_text(face="bold"), 
+        plot.title = element_text(size = 18, face = "bold")) +  
+  ggtitle("Arrests Per Year as a Percentage of Population")
 
   
